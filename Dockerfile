3c1d7d0b0bf7c100a755052fac8317cb6a951493
basharE <bashar.egbariya@peer39.com>
2021-03-28 17:49:11 +0300

    using postgres db instead of mysql

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.7-alpine
+FROM python:3.8-slim-buster
 ADD . /app
 WORKDIR /app
-RUN pip3 install flask requests selenium pymysql
+RUN pip3 install flask requests selenium psycopg2-binary
 EXPOSE 5000
 VOLUME /app/logs
 CMD python3 rest_app.py
--- db_connector.py
+++ db_connector.py
@@ -1,28 +1,26 @@
-import pymysql
 import datetime
+import psycopg2
 
 
 # Post Method to get as variables, user name and
 # user id as input
 # and Insert them into users table
 # Returns true if Insertion succeeded, false if not
-from pip._vendor.retrying import retry
-
 
 def post_user(user_name, user_id):
-    con = pymysql.connect(host='db', port=3307, user='user', password='password', db='db')
-    con.autocommit(True)
+    con = psycopg2.connect(database="db", user="docker_user", password="password", host="database", port=5432)
     cur = con.cursor()
     current_date_time = datetime.datetime.now()
-    sql = "INSERT into db.USERS (user_name, user_id, creation_date) VALUES " \
+    sql = "INSERT into USERS (user_name, user_id, creation_date) VALUES " \
           "('" + user_name + "'," + user_id + ",'" + str(current_date_time) + "')"
 
     # using prepared statements (Extras sec.2)
-    stmt = "INSERT into db.USERS (user_name, user_id, creation_date) VALUES (%s,%s,%s)"
+    stmt = "INSERT into USERS (user_name, user_id, creation_date) VALUES (%s,%s,%s)"
     insert_tuple = (user_name, user_id, str(current_date_time))
     try:
         # cur.execute(sql)
         cur.execute(stmt, insert_tuple)
+        con.commit()
         return True
     except:
         return False
@@ -33,16 +31,15 @@ def post_user(user_name, user_id):
 
 # Get Method to get as variable, user id as input
 # and return user name if exist and empty string if not
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_delay=30000)
+# @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_delay=30000)
 def get_user(user_id):
-    con = pymysql.connect(host='db', port=3307, user='user', password='password', db='db')
+    con = psycopg2.connect(database="db", user="docker_user", password="password", host="database", port=5432)
     cur = con.cursor()
-    sql = "SELECT user_name FROM db.USERS WHERE user_id=" + user_id
+    sql = "SELECT user_name FROM USERS WHERE user_id=" + user_id + ";"
     try:
         cur.execute(sql)
         return cur.fetchall()
     except:
-        raise Exception("Retry!")
         return False
     finally:
         cur.close()
@@ -53,12 +50,12 @@ def get_user(user_id):
 # as input, and update the user name for
 # specific id if exist by the new user name
 def update_user(user_name, user_id):
-    con = pymysql.connect(host='db', port=3307, user='user', password='password', db='db')
-    con.autocommit(True)
+    con = psycopg2.connect(database="db", user="docker_user", password="password", host="database", port=5432)
     cur = con.cursor()
-    sql = "UPDATE db.USERS SET user_name = '" + user_name + "' WHERE user_id = " + user_id
+    sql = "UPDATE USERS SET user_name = '" + user_name + "' WHERE user_id = " + user_id + ";"
     try:
         cur.execute(sql)
+        con.commit()
         return cur.fetchall()
     except:
         return False
@@ -70,12 +67,12 @@ def update_user(user_name, user_id):
 # Delete Method to accept user id as input
 # and deletes the user from db if exist
 def delete_user(user_id):
-    con = pymysql.connect(host='db', port=3307, user='user', password='password', db='db')
-    con.autocommit(True)
+    con = psycopg2.connect(database="db", user="docker_user", password="password", host="database", port=5432)
     cur = con.cursor()
-    sql = "DELETE FROM db.USERS WHERE user_id =" + user_id
+    sql = "DELETE FROM USERS WHERE user_id =" + user_id + ";"
     try:
         cur.execute(sql)
+        con.commit()
         return cur.fetchall()
     except:
         return False
